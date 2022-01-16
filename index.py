92bec981105ab59a97fd5e8879e4a8484896922e
lucaslamdev <50436012+lucaslamdev@users.noreply.github.com>
2022-01-16 17:55:34 -0300

    Update index.py

--- index.py
+++ index.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
- 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+
+@app.route("/teste")
+def hello_world():
+    return "<p>Hello, teste!</p>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
