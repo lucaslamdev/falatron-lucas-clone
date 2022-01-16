59c7d82214c081e99dba1bbb5b63d5732e720ae4
lucaslamdev <50436012+lucaslamdev@users.noreply.github.com>
2022-01-16 18:08:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,5 @@ def hello_world():
 def hello_world():
     return "<p>Hello, teste!</p>"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
